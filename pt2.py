a5dc968152b798f4490dbcd1041972a57d68c20b
avasavino <avasavino@ufl.edu>
2025-03-14 11:26:53 -0400

    Update pt2.py

--- pt2.py
+++ pt2.py
@@ -6,9 +6,9 @@ def get_complex_grid(
 ) -> np.ndarray:
     ...
 #create array of real values starting from real part of top_left to bottom_right
-    real_values = np.arrange(top_left[0], bottom_right[0], step)
+    real_values = np.arange(top_left.real, bottom_right.imag, step)
 #create array of imaginary values starting from imaginary part of top_left to bottom_right (reversed so order is top to bottom still)
-    imaginary_values = np.arrange(top_left[1], bottom_right[1], step[::1])
+    imaginary_values = np.arange(top_left.real, bottom_right.imag, step[::-1])
 # generate 2D grid
     complex_grid = np.array([[r + 1j * i for r in real_values] for i in imaginary_values])
     return complex_grid
