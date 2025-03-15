c3384eb1543db1089d704f2334797f4f7ae57d3c
avasavino <avasavino@ufl.edu>
2025-03-15 16:41:13 -0400

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -37,3 +37,18 @@ def get_escape_time_color_arr(
             color_arr[i, j] = (max_iterations - escape_time + 1) / (max_iterations + 1)
 
     return color_arr
+
+
+def get_complex_grid(
+    top_left: complex,
+    bottom_right: complex,
+    step: float
+) -> np.ndarray:
+    ...
+#create array of real values starting from real part of top_left to bottom_right
+    real_values = np.arange(top_left.real, bottom_right.imag, step)
+#create array of imaginary values starting from imaginary part of top_left to bottom_right (reversed so order is top to bottom still)
+    imaginary_values = np.arange(top_left.real, bottom_right.imag, step[::-1])
+# generate 2D grid
+    complex_grid = np.array([[r + 1j * i for r in real_values] for i in imaginary_values])
+    return complex_grid
