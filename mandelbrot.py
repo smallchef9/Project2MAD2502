82e8d2e3dca5b206afe49e101412b0c81f63b316
njupalli <nehajupalli06@gmail.com>
2025-03-15 19:02:07 -0400

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -34,7 +34,7 @@ def get_escape_time_color_arr(
     for i in range(c_arr.shape[0]):
         for j in range(c_arr.shape[1]):
             escape_time = get_escape_time(c_arr[i, j], max_iterations)
-            if escape_time == none:
+            if escape_time == None:
                 escape_time == max_iterations
             color_arr[i, j] = (max_iterations - escape_time + 1) / (max_iterations + 1)
 
